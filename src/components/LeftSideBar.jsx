2ef7840629b9c1c143d5964ea008dfd42df46ee5
Aleksandr <snt17gm@gmail.com>
2023-09-26 02:06:16 +0300

    initial commit :)

---
+++ src/components/LeftSideBar.jsx
@@ -0,0 +1,11 @@
+import '../styles/LedtSideBar.css'
+const LeftSideBar = () => {
+    return (
+     <>
+     <div className="filter">
+         Filter
+     </div>
+     </>
+    );
+};
+export default LeftSideBar;
---
+++ src/components/WorkesTable.jsx
@@ -0,0 +1,10 @@
+import '../styles/WorkersTable.css'
+export const WorkesTable = () => {
+    return (
+        <>
+            <div className="table">
+                table
+            </div>
+        </>
+    );
+};
